b1b2e068a0fc54b57991db737801435f7404aadb
Rakesh <rbandi211570@gmail.com>
2021-12-15 17:46:45 -0800

    I should commit more often... at this point, I have a spreadsheet that takes in input, and resizing columns/rows works. TODO: change history table for redo/undo, that can ALSO be used in future autoSave() to propagate table state to server. For each table entry, place a transparent div on top which will require a double-click to disappear and enable entry input; clicking away re-enables the div. Also an selected[] to store ctrl/shift selections of multiple entries. Copy-paste, format bar w/ all sorts of formatting options, persistent storage... maybe more features

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { MenuPanel, FormatPanel, SpreadSheetPanel} from './components'
 import reportWebVitals from './reportWebVitals';
+
+function App() {
+  return (
+    <div>
+      <div className="header" style={{ height: window.innerHeight * .05 }}>
+          Lifestyle Trackers
+      </div>
+      <FormatPanel />
+      <div id="pageID" className="page">
+        <MenuPanel />
+        {/*<SpreadSheetPanel outerHeight='200' outerWidth='200' cols='5' rows='5' loadedSheet='<p>Loaded</p>'/>*/}
+        <SpreadSheetPanel outerHeight='600' outerWidth='600' defaultRowHeight='20' defaultColWidth='100' cols='5' rows='9' loadedSheet=''/>
+      </div>
+    </div>
+
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,3 +32,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
