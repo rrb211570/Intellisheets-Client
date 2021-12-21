defdbb447925952bb0573d8864cb83678c384bf7
Rakesh <rbandi211570@gmail.com>
2021-12-20 22:08:16 -0800

    see comment of next commit

--- my-app/src/components/SpreadSheetPanel/SpreadSheetPanel.js
+++ my-app/src/components/SpreadSheetPanel/SpreadSheetPanel.js
@@ -3,6 +3,9 @@ import applyResizers from './resizers.js'
 
 let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let newSheet = false;
+const NOCOMMAND = 0;
+const CONTROL = 1;
+const SHIFT = 2;
 
 class SpreadSheetPanel extends React.Component {
     constructor(props) {
@@ -15,11 +18,18 @@ class SpreadSheetPanel extends React.Component {
                 loadedSheet(this.props.loadedSheet) :
                 defaultSheet(parseInt(this.props.rows), parseInt(this.props.defaultRowHeight), parseInt(this.props.cols), parseInt(this.props.defaultColWidth)),
             tableEntries: [],
-            changeHistory: []
+            changeHistory: [new Map()],
+            changeHistoryIndex: 0,
+            keyEventState: NOCOMMAND,
         }
         this.setSheetDimensions = this.setSheetDimensions.bind(this);
         this.getSheetDimensions = this.getSheetDimensions.bind(this);
-        this.setTable = this.setTable.bind(this);
+        this.getTable = this.getTable.bind(this);
+        this.recordChange = this.recordChange.bind(this);
+        this.keyPressed = this.keyPressed.bind(this);
+        this.keyUpped = this.keyUpped.bind(this);
+        this.undo = this.undo.bind(this);
+        this.redo = this.redo.bind(this);
     }
     componentDidMount() {
         let borderWidth = 2;
@@ -27,24 +37,108 @@ class SpreadSheetPanel extends React.Component {
             (sum, entry) => sum + parseInt(entry.style.height), 0) + 2 * borderWidth;
         let width = parseInt(document.getElementById('row0').style.width) + 2 * borderWidth;
         this.setSheetDimensions(height, width);
-        applyResizers(height, width, this.getSheetDimensions, this.setSheetDimensions, this.setTable); // util.js function
+        applyResizers(height, width, this.getSheetDimensions, this.setSheetDimensions, this.recordChange); // util.js function
     }
     setSheetDimensions(height, width) {
         this.setState({
-            tableHeight: height,
-            tableWidth: width
-        })
+            tableHeight: height != null ? height : this.state.tableHeight,
+            tableWidth: width !== null ? width : this.state.tableWidth
+        });
     }
     getSheetDimensions() {
         return [this.state.tableHeight, this.state.tableWidth];
     }
-    setTable() {
+    getTable() {
         return this.state.table;
+    }
+    // entries = [ row, col, prevVal, newVal], [entry, prevVal, newVal], ... ]
+    recordChange(entries) {
+        let prevState = new Map();
+        let newState = new Map();
+        entries.forEach((entry) => {
+            if (!this.state.changeHistory[this.state.changeHistoryIndex].has(`${entry[0]}${entry[1]}`)) {
+                prevState.set(`${entry[0]}${entry[1]}`, entry[2]);
+            }
+            newState.set(`${entry[0]}${entry[1]}`, entry[3]);
+        });
+        this.state.changeHistory[this.state.changeHistoryIndex].forEach((val, entry) => prevState.set(entry, val));
+        let arr = [...this.state.changeHistory.slice(0, this.state.changeHistoryIndex)];
+        arr.push(prevState);
+        arr.push(newState);
+        this.setState({
+            changeHistory: arr,
+            changeHistoryIndex: this.state.changeHistoryIndex + 1,
+        });
+        console.log(this.state.changeHistory);
+        console.log(this.state.changeHistoryIndex);
+    }
+    keyPressed(e) {
+        switch (this.state.keyEventState) {
+            case NOCOMMAND:
+                if (e.key == 'Control') {
+                    console.log('Control');
+                    this.setState({ keyEventState: CONTROL });
+                }
+                if (e.key == 'Shift') {
+                    console.log('Shift');
+                    this.setState({ keyEventState: SHIFT });
+                }
+                break;
+            case CONTROL:
+                if (e.key == 'z') {
+                    this.undo();
+                }
+                if (e.key == 'y') {
+                    this.redo();
+                }
+                break;
+            case SHIFT:
+                break;
+        }
+    }
+    keyUpped(e) {
+        switch (this.state.keyEventState) {
+            case NOCOMMAND:
+                console.log('NO COMMAND UP');
+                break;
+            case CONTROL:
+            case SHIFT:
+                if (e.key == 'Control' || e.key == 'Shift') {
+                    console.log('CTRL/SHIFT UP');
+                    this.setState({ keyEventState: NOCOMMAND });
+                }
+                break;
+        }
+    }
+    undo() {
+        console.log('Undo');
+        if (this.state.changeHistoryIndex > 0) {
+            for (const [key, value] of this.state.changeHistory[this.state.changeHistoryIndex - 1].entries()) {
+                console.log(key);
+                if (/^\.row.null$/.test(key)) {
+                    let entry = document.getElementById(key.match(/row./));
+                    applyState(entry, value);
+                } else if (/^nullnull$/.test(key)) {
+                    let entry = document.getElementById('spreadsheet');
+                    applyState(entry, value, this.setSheetDimensions);
+                } else {
+                    let entry = document.querySelector(key.match(/^\.row.\.col.$/));
+                    applyState(entry, value);
+                }
+            }
+            this.setState({ changeHistoryIndex: this.state.changeHistoryIndex - 1 });
+        }
+    }
+    redo() {
+        console.log('Redo');
+        if (this.state.changeHistoryIndex != this.state.changeHistory.length - 1) {
+            //this.setState({changeHistoryIndex: this.state.changeHistoryIndex+1});
+        }
     }
     render() {
         return (
             <div className="content" id="contentID" style={{ height: this.props.outerHeight + 'px', width: this.props.outerWidth + 'px' }}>
-                <div id='spreadsheet' key='eh' style={{ height: `${this.state.tableHeight}px`, width: `${this.state.tableWidth}px`, border: '1px solid black' }}>
+                <div id='spreadsheet' tabIndex='-1' onKeyDown={this.keyPressed} onKeyUp={this.keyUpped} style={{ height: `${this.state.tableHeight}px`, width: `${this.state.tableWidth}px`, border: '1px solid black' }}>
                     {this.state.table}
                 </div>
             </div>
@@ -52,6 +146,38 @@ class SpreadSheetPanel extends React.Component {
     }
 }
 export default SpreadSheetPanel;
+
+function applyState(entry, styleArr, func) {
+    if (styleArr[0] != null) entry.value = styleArr[0];
+    styleArr[1].forEach(([property, value]) => {
+        console.log(property + ' ' + value);
+        switch (property) {
+            case 'height':
+                entry.style.height = value + 'px';
+                if ([...entry.classList].filter(name => /^row0$/.test(name)).length == 0 &&
+                    [...entry.classList].filter(name => /^col0$/.test(name)).length == 0) {
+                    entry.querySelector('.input').style.height = value - 4 + 'px';
+                    entry.querySelector('#cover').style.height = value + 'px';
+                }
+                break;
+            case 'width':
+                entry.style.width = value + 'px';
+                if (typeof func !== "undefined") {
+                    func(null,value);
+                }
+                if ([...entry.classList].filter(name => /^row0$/.test(name)).length == 0 &&
+                    [...entry.classList].filter(name => /^col0$/.test(name)).length == 0 &&
+                    [...entry.classList].filter(name => /^col.$/.test(name)).length != 0) {
+                    entry.querySelector('.input').style.width = value - 4 + 'px';
+                    entry.querySelector('#cover').style.width = value + 'px';
+                }
+                break;
+            case 'marginLeft':
+                entry.style.marginLeft = value + 'px';
+                break;
+        }
+    });
+}
 
 function loadedSheet(loadedSheet) {
     return (
@@ -76,7 +202,7 @@ const defaultSheet = (rows, defaultHeight, cols, defaultWidth) => {
             <p>{i + 1}</p>
         </div>);
         for (let j = 1; j < cols + 1; ++j) {
-            row.push(<div className={`row${i + 1} col${j} entry`} style={{ height: `${defaultHeight}px`, width: `${defaultWidth}px`, marginLeft: `${defaultWidth * (j - 1) + defaultWidth / 2}px` }}><input className='input' type='text' style={{ height: `${defaultHeight-4}px`, width: `${defaultWidth-4}px`, border: 'none'}}></input></div>)
+            row.push(<div className={`row${i + 1} col${j} entry`} style={{ height: `${defaultHeight}px`, width: `${defaultWidth}px`, marginLeft: `${defaultWidth * (j - 1) + defaultWidth / 2}px` }}><input onBlur={showCoverEvent} onKeyUp={stopPropagation} onKeyDown={stopPropagation} className='input' type='text' style={{ height: `${defaultHeight - 4}px`, width: `${defaultWidth - 4}px`, border: 'none' }}></input><div id='cover' tabIndex='-1' onClickCapture={selectCell} onBlur={unselectCell} onDoubleClickCapture={hideCoverEvent} style={{ position: 'absolute', left: '0', top: '0', height: `${defaultHeight}px`, width: `${defaultWidth}px`, border: 'none' }}></div></div>)
         }
         rowsArr.push(row);
     }
@@ -90,4 +216,26 @@ const defaultSheet = (rows, defaultHeight, cols, defaultWidth) => {
     }
 
     return table;
+}
+
+function stopPropagation(e) {
+    e.stopPropagation();
+}
+
+function selectCell(e) {
+    e.target.focus();
+    e.target.style.border = '2px solid blue';
+}
+
+function unselectCell(e) {
+    e.target.style.border = 'none';
+}
+
+function hideCoverEvent(e) {
+    e.target.style.zIndex = -1;
+    e.target.parentElement.querySelector('.input').focus();
+}
+
+function showCoverEvent(e) {
+    e.target.parentElement.querySelector('#cover').style.zIndex = 1;
 }
--- my-app/src/components/SpreadSheetPanel/resizers.js
+++ my-app/src/components/SpreadSheetPanel/resizers.js
@@ -1,74 +1,47 @@
-function applyResizers(height, width, getSheetDimensions, setSheetDimensions, funct1) {
-    fixResizers('col', height, width, getSheetDimensions, setSheetDimensions, funct1);
-    fixResizers('row', height, width, getSheetDimensions, setSheetDimensions, funct1);
+function applyResizers(height, width, getSheetDimensions, setSheetDimensions, recordChange) {
+    fixResizers('col', height, width, getSheetDimensions, setSheetDimensions, recordChange);
+    fixResizers('row', height, width, getSheetDimensions, setSheetDimensions, recordChange);
 }
 
-function fixResizers(axis, height, width, getSheetDimensions, setSheetDimensions, funct1) {
+function fixResizers(axis, height, width, getSheetDimensions, setSheetDimensions, recordChange) {
     let table = document.getElementById('spreadsheet');
-    let axisEntries = [];;
-    if (axis === 'col') axisEntries = [...table.querySelectorAll('.AxisX')];
-    else {
-        axisEntries = [...table.querySelectorAll('.col0')];
-    }
-    axisEntries.reduce((NULL, entry) => {
+    let axisEntries = axis === 'col' ? [...table.querySelectorAll('.AxisX')] : [...table.querySelectorAll('.col0')];
+    axisEntries.forEach(entry => {
         const resizer = document.createElement('div');
         resizer.classList.add('resizer-' + (axis === 'col' ? 'top' : 'left'));
-        if (axis === 'col') resizer.style.height = `${height}px`;
-        else resizer.style.width = `${width}px`;
+        axis === 'col' ? resizer.style.height = `${height}px` : resizer.style.width = `${width}px`;
         entry.appendChild(resizer);
-
-        createResizableColumn(entry, axis, getSheetDimensions, setSheetDimensions, funct1);
+        createResizableColumn(entry, axis, resizer, getSheetDimensions, setSheetDimensions, recordChange);
     });
-
 }
 
-function createResizableColumn(entry, axis, getSheetDimensions, setSheetDimensions, funct1) {
+function createResizableColumn(entry, axis, resizer, getSheetDimensions, setSheetDimensions, recordChange) {
     let x = 0;
     let y = 0;
     let h = 0;
     let w = 0;
-    let colMarginsLeft = [];
-    let rowHeights = [];
-    let rowWidths = [];
     let spreadSheetDimensions = [];
+    let entries = [];
+    let colMarginsLeft = [];
+    let rowWidth = 0;
+    let changeOccurred = false;
     let table = document.getElementById('spreadsheet');
-    let resizer = entry.querySelector(axis === 'col' ? '.resizer-top' : '.resizer-left');
 
     const mouseDownHandler = function (e) {
         spreadSheetDimensions = getSheetDimensions();
         if (axis === 'col') {
             x = e.clientX;
             w = parseInt(window.getComputedStyle(entry).width, 10);
-            // get colWidths
-            let colNum = [...entry.classList].filter(name => /^col.$/.test(name))[0].slice(-1);
-            for (let i = 0; i < colNum; ++i) colMarginsLeft.push(null);
-            while (1) {
-                let arr = [...table.querySelectorAll(`.col${colNum}`)];
-                if (arr.length == 0) break;
-                colMarginsLeft.push(parseInt(arr[0].style.marginLeft, 10));
-                colNum++;
-            }
-            let rowIndex = 0;
-            while (1) {
-                let arr = [...table.querySelectorAll(`#row${rowIndex}`)];
-                if (arr.length == 0) break;
-                rowWidths.push(parseInt(arr[0].style.width, 10));
-                rowIndex++;
-            }
+            let colNum = parseInt([...entry.classList].filter(name => /^col.$/.test(name))[0].slice(-1));
+            addEntries(entries, axis, colNum, [null, w],spreadSheetDimensions); // store current state
+            storeColMargins(colMarginsLeft, colNum);
+            rowWidth = parseInt(document.getElementById('row0').style.width, 10);
         } else {
             y = e.clientY;
             h = parseInt(window.getComputedStyle(entry).height, 10);
-            // get rowHeights
             let rowNum = [...entry.classList].filter(name => /^row.$/.test(name))[0].slice(-1);
-            for (let i = 0; i < rowNum; ++i) rowHeights.push(null);
-            while (1) {
-                let arr = [...table.querySelectorAll(`.row${rowNum}Entry`)];
-                if (arr.length == 0) break;
-                rowHeights.push(parseInt(arr[0].style.marginTop, 10));
-                rowNum++;
-            }
+            addEntries(entries, axis, rowNum, [h, null],spreadSheetDimensions); // store current state
         }
-
         document.addEventListener('mousemove', mouseMoveHandler);
         document.addEventListener('mouseup', mouseUpHandler);
         resizer.classList.add('resizing-' + (axis === 'col' ? 'top' : 'left'));
@@ -77,50 +50,21 @@ function createResizableColumn(entry, axis, getSheetDimensions, setSheetDimensio
     // disable resizing if table.width == content.width
     const mouseMoveHandler = function (e) {
         if (axis === 'col') {
-            const preDx = e.clientX - x;
-            const dx = w + preDx < 0 ? -w + 1 : preDx; // set dx so as to maintain 1 pixel minimum width
-            let colNum = [...entry.classList].filter(name => /^col.$/.test(name))[0].slice(-1);
-            let idx = colNum;
-
-            let cols = [];
-            for (let i = 0; i < idx; ++i) cols.push(null);
-            while (1) {
-                let arr = [...table.querySelectorAll(`.col${idx}`)];
-                if (arr.length == 0) break;
-                cols.push(arr);
-                idx++;
-            }
-            cols[colNum].forEach(box => { //resize selected col
-                box.style.width = w + dx + 'px';
-            });
-            cols[colNum].slice(1).forEach(cell => {
-                cell.querySelector('.input').style.width = w + dx - 4 + 'px';
-            });
-            idx = parseInt(colNum) + 1
-            cols.slice(parseInt(colNum) + 1).forEach(col => { //percolate margins to all cols on the right
-                col.forEach(box => box.style.marginLeft = colMarginsLeft[idx] + dx + 'px');
-                idx++;
-            });
-            rowWidths.forEach((width, index) => {
-                document.getElementById(`row${index}`).style.width = width + dx + 'px';
-            });
+            changeOccurred = x != e.clientX ? true : false;
+            const dx = w + e.clientX - x < 0 ? -w + 1 : e.clientX - x; // set dx so as to maintain 1 pixel minimum width
+            let colNum = parseInt([...entry.classList].filter(name => /^col.$/.test(name))[0].slice(-1), 10);
+            updateWidths(colNum, w, rowWidth, dx);
+            updateColMargins(colNum + 1, colMarginsLeft, dx);
             let sheetWidth = spreadSheetDimensions[1] + dx;
-            setSheetDimensions(getSheetDimensions()[0], sheetWidth);
+            setSheetDimensions(spreadSheetDimensions[0], sheetWidth);
             table.querySelectorAll('.resizer-left').forEach(resizerDiv => resizerDiv.style.width = `${sheetWidth}px`);
         } else {
-            const preDy = e.clientY - y;
-            const dy = h + preDy < 0 ? -h + 1 : preDy; // set dy so as to maintain 1 pixel minimum height
+            changeOccurred = y != e.clientY ? true : false;
+            const dy = h + e.clientY - y < 0 ? -h + 1 : e.clientY - y; // set dy so as to maintain 1 pixel minimum height
             let rowNum = [...entry.classList].filter(name => /^row.$/.test(name))[0].slice(-1);
-            let arr = [...table.querySelectorAll(`.row${rowNum}`)];
-            arr[1].style.lineHeight = h + dy + 'px';
-            arr.forEach(box => {
-                box.style.height = h + dy + 'px';
-            });
-            arr.slice(2).forEach(cell => {
-                cell.querySelector('.input').style.height = h + dy - 4 + 'px';
-            });
+            updateHeights(rowNum, h, dy);
             let sheetHeight = spreadSheetDimensions[0] + dy;
-            setSheetDimensions(sheetHeight, getSheetDimensions()[1]);
+            setSheetDimensions(sheetHeight, spreadSheetDimensions[1]);
             table.querySelectorAll('.resizer-top').forEach(resizerDiv => resizerDiv.style.height = `${sheetHeight}px`);
         }
     };
@@ -129,13 +73,107 @@ function createResizableColumn(entry, axis, getSheetDimensions, setSheetDimensio
         document.removeEventListener('mousemove', mouseMoveHandler);
         document.removeEventListener('mouseup', mouseUpHandler);
         resizer.classList.remove('resizing-' + (axis === 'col' ? 'top' : 'left'));
+        if (changeOccurred) {
+            if (axis == 'col') {
+                let colNum = parseInt([...entry.classList].filter(name => /^col.$/.test(name))[0].slice(-1), 10);
+                let len = table.querySelectorAll(`.col${colNum}`).length;
+                let newWidth = parseInt(table.querySelector(`.col${colNum}`).style.width, 10);
+                for (let i = 0; i < len; ++i) {
+                    entries[i].push([null, [['width', newWidth]]]);
+                }
+                for (let i = 1; i < (entries.length - 1) / len; ++i) {
+                    let newMarginLeft = parseInt(table.querySelector(`.col${colNum + i}`).style.marginLeft, 10);
+                    for (let j = 0; j < len; ++j) {
+                        entries[len * i + j].push([null, [['marginLeft', newMarginLeft]]]);
+                    }
+                }
+                entries[len * 2].push([null, [['width', rowWidth + parseInt(entry.style.width,10) - w+4]]]);
+                console.log(rowWidth+' '+entry.style.width+' '+w);
+                for (let i = 0; i < len; ++i) {
+                    entries.push([`.row${i}`, null, [null, [['width', rowWidth]]], [null, [['width', rowWidth + entry.style.width - w]]]]);
+                }
+            } else {
+                let rowNum = [...entry.classList].filter(name => /^row.$/.test(name))[0].slice(-1);
+                let newHeight = table.querySelector(`.row${rowNum}`).style.height;
+                for (let i = 0; i < entries.length - 1; ++i) {
+                    entries[i].push([null, [['height', newHeight]]]);
+                }
+                entries[entries.length - 1].push([null, [['height', spreadSheetDimensions[0] + entry.style.height - h+4]]]);
+            }
+            recordChange(entries);
+            changeOccurred = false;
+        }
         colMarginsLeft = [];
-        rowHeights = [];
-        rowWidths = [];
         spreadSheetDimensions = [];
+        entries = [];
     };
 
     resizer.addEventListener('mousedown', mouseDownHandler);
+}
+
+function addEntries(entries, axis, index, [height, width],[sheetHeight, sheetWidth]) {
+    let table = document.getElementById('spreadsheet');
+    if (axis === 'col') {
+        entries.push(...[...table.querySelectorAll(`.col${index}`)].map((val, idx) => {
+            return [`.row${idx}`, `.col${index}`, [null, [['width', width]]]];
+        }));
+        let arr = [];
+        while ((arr = [...table.querySelectorAll(`.col${++index}`)]).length != 0) {
+            entries.push(...arr.map((val, idx) => {
+                return [`.row${idx}`, `.col${index}`, [null, [['marginLeft', parseInt(val.style.marginLeft)]]]];
+            }));
+        }
+        entries.push([null, null, [null, [['width', sheetWidth]]]]);
+    } else {
+        [...table.querySelectorAll(`.row${index}`)].slice(1).forEach((val, idx) => {
+            entries.push([`.row${index}`, `.col${idx}`, [null, [['height', height]]]]);
+        });
+        entries.push([`.row${index}`, null, [null, [['height', height]]]]);
+        entries.push([null, null, [null, [['height', sheetHeight]]]]);
+    }
+}
+
+function storeColMargins(colMarginsLeft, colNum) {
+    let table = document.getElementById('spreadsheet');
+    let elem = null;
+    while ((elem = table.querySelector(`.col${++colNum}`)) != null) {
+        colMarginsLeft.push(parseInt(elem.style.marginLeft, 10));
+    }
+}
+
+function updateHeights(rowIndex, height, dy) {
+    let table = document.getElementById('spreadsheet');
+    let arr = [...table.querySelectorAll(`.row${rowIndex}`)];
+    arr[1].style.lineHeight = height + dy + 'px';
+    arr.forEach((cell, index) => {
+        if (index > 1) {
+            cell.querySelector('.input').style.height = height + dy - 4 + 'px';
+            cell.querySelector('#cover').style.height = height + dy + 'px';
+        }
+        cell.style.height = height + dy + 'px';
+    });
+}
+
+function updateWidths(colIndex, width, rowWidth, dx) {
+    let table = document.getElementById('spreadsheet');
+    let arr = [...table.querySelectorAll(`.col${colIndex}`)];
+    arr.forEach((cell, index) => {
+        if (index > 0) {
+            cell.querySelector('.input').style.width = width + dx - 4 + 'px';
+            cell.querySelector('#cover').style.width = width + dx + 'px';
+        }
+        cell.style.width = width + dx + 'px';
+        document.getElementById(`row${index}`).style.width = rowWidth + dx + 'px';
+    });
+}
+
+function updateColMargins(colIndex, colMarginsLeft, dx) {
+    let table = document.getElementById('spreadsheet');
+    let idx = colIndex - 1;
+    let arr = [];
+    while ((arr = [...table.querySelectorAll(`.col${++idx}`)]).length != 0) {
+        arr.forEach(box => box.style.marginLeft = colMarginsLeft[idx - colIndex] + dx + 'px');
+    }
 }
 
 export default applyResizers;
