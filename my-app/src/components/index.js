5a637ca208a406c29d4c3b4e7308d3c53aecfc5c
Rakesh <rbandi211570@gmail.com>
2022-01-06 19:55:23 -0800

    Refactored the code a lot more. I found a way to separate functions bound to React state, and move them outside the component body while maintaining the binding. I chose to do this with a bunch of functions, one of them being redo(). However, redo() started acting weirdly afterwards, and I felt frustrated by having to debug features that I already thought worked correctly. So I started building some automated tests, partly to practice doing QA (no, I am not using promises or anything.. just old fashioned console logs), and also b/c I want to reduce lead time on debugging by having a test which tells me what works, so I know where to look for what does not work. Currently there is an automated test for col/row resizing, which could be scaled to test functional reliability for big n of rows/cols (and maybe provides QA). But it is not particularly useful to me in debugging (well maybe it will be if I refactor and somehow break logic that makes the 10th resizing not work, but idk). The next tests I will build will check that resize and textChange events correctly update changeHistory[] and collectedData[]. Then I can test that undo correctly traverses changeHistory[] and updates collectedData[] before copy/pasting the test logic into redoTest. This should give me enough starter code that will save me headaches as I build more features that connect with changeHistory[] and collectedData[]. Maybe, as I update and build on the tests, I will learn how to tailor them to give me more useful debugging output

--- my-app/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import MenuPanel from './MenuPanel.js'
-import FormatPanel from './FormatPanel.js'
-import GridPanel from './GridPanel.js'
-import SpreadSheetPanel from './SpreadSheetPanel/SpreadSheetPanel.js'
-
-export {MenuPanel,FormatPanel,GridPanel,SpreadSheetPanel};
